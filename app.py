65169f2cd2d81476242272e9cb3ecfc759f9d5a6
Felipe Gomez <fgomezcode@gmail.com>
2022-12-27 03:29:49 -0800

    Search added- debugging on add/remove card.

--- app.py
+++ app.py
@@ -3,153 +3,60 @@ import json
 
 app = Flask(__name__)
 
-
-def collected(keys):
-    cards = get_cards()
-    for id in cards:
-        js = 'static/Decks/Cards.json'
-        with open(js, "r", encoding='utf-8') as jsonFile:
-            cards = json.load(jsonFile)
-
-        if id in keys:
-            cards[id]["collected"] = "true"
-        else:
-            cards[id]["collected"] = "false"
-        with open(js, "w", encoding='utf-8') as jsonFile:
-            json.dump(cards, jsonFile, ensure_ascii=False, indent=4)
-
-
 def get_cards():
     js = 'static/Decks/Cards.json'
     f = json.load(open(js, 'r', encoding='utf-8'))
     return f
 
-
-@app.route('/collection')
+@app.route('/collection', methods=['POST', 'GET'])
 def collection():
-
     if request.method == 'POST':
         return render_template('collection.html')
 
     if request.method == 'GET':
         return render_template('collection.html')
-
 
 # ========================================
 @app.route('/', methods=['POST', 'GET'])
 def home():  # put application's code here
     if request.method == 'GET':
-        print('get')
-
-    if request.method == 'POST':
-
         current_collection = request.form
-        card_keys = current_collection['collection'].split(',')
-        card_keys.sort()
-    try:
-        cards = get_cards()
-        results = {}
-        for i in card_keys:
-            results[i] = cards[i]
-
-    except:
-        results = {}
+        print(current_collection)
         return render_template('base.html', cards=get_cards())
 
+    if request.method == 'POST':
+        current_collection = request.form
+        print(current_collection)
+        return render_template('base.html', cards=get_cards())
 
 # ========================================
 @app.route('/cards', methods=['POST', 'GET'])
 def load_cards():
+    search_query = None
+    cards_in_collection = None
     if request.method == 'GET':
-        return render_template('base.html')
+        search = request.form
+        try:
+            data= search['query']
+            print(data,search)
+        except KeyError:
+            data = search['search_bar'].capitalize()
+
+        return render_template('cards.html', deck=data)
 
     if request.method == 'POST':
-        search_query = request.form
+        search = request.form
+        try:
+            data = search['query']
+            print(data,search)
+        except KeyError:
+            data = search['search_bar'].capitalize()
 
-    cards_data = get_cards()
+        return render_template('cards.html', deck=data)
 
-    results = {}
-
-    if 'query' in search_query.keys():
-        if search_query['query'] != '':
-            deck_search = (search_query['query'])
-            for card in cards_data:
-                if deck_search in card:
-                    results[card] = cards_data[card]
-            return render_template('cards.html', cards=results)
-
-    if 'search_bar' in search_query.keys():
-        if search_query['search_bar'] != '':
-            search_bar = (search_query['search_bar'])
-            for key, value in cards_data.items():
-                if search_bar.capitalize() in value['name']:
-                    results[key] = value
-            return render_template('cards.html', cards=results)
-
-    return render_template('cards.html', cards=cards_data)
 
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
 
 
-
-'''
-
-
-<!--
-<h1>COLLECTION</h1>
-
-    <div class="row">
-    {% for i in cards.keys()%}
-        <div id="{{i}}" class=" card collected col-sm-4 col-lg-2">
-            <div class="card-header">
-                {{cards[i]["card_code"]}}
-                {{cards[i]["name"]}}
-            </div>
-            <div class="card-image">
-                <img class="img-thumbnail popover-image" data-bs-toggle="modal" data-bs-target="#{{i}}-Modal" src="static/Decks/{{cards[i]['deck']}}/CardImages/{{cards[i]['images']}}" alt="{{i}}">
-            </div>
-            <div class="card-footer">
-                <div class="row">
-                    <div class="col text-center">
-
-                          <form action="/collection" method="post">
-                          <button onclick="collection_remove_from_local_data('{{i}}')" id="collection-button" class="btn btn-danger" name="collection"  >REMOVE</button>
-                          </form>
-                    </div>
-                </div>
-            
-        </div>
-
- Modal
-    <div class="modal fade" id="{{i}}-Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-
-          <div class="modal-header">
-            <h5 class="modal-title" id="ModalLabel">{{cards[i]["card_code"]}} {{cards[i]["name"]}}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-
-          <div class="modal-body">
-                <img class="img-thumbnail popover-image" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                     src="static/Decks/{{cards[i]['deck']}}/CardImages/{{cards[i]['images']}}" alt="">
-                <div class="card-text">
-                    <p>{{cards[i]["text"]}}</p>
-                </div>
-          </div>
-
-            <div class="card-footer">
-                <div class="row">
-                    <div class="col text-center">
-                        <
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
-    </div>
-    {% endfor %}
-    </div>
--->'''
--- static/index.js
+++ static/index.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('static/Decks/Cards.json')
   .then((response) => response.json())
   .then((data) => store_card_json(data));
+
+    create_collection();
 });
 
 function store_card_json(cards){
@@ -11,13 +13,17 @@ function store_card_json(cards){
     }
 }
 
+function add_to_local_data(id, card){
+    let cards = JSON.parse(localStorage['CardData'])
+    console.log(id)
+    cards[id]['collected'] = 'true'
+    card['collected'] = 'true'
 
-function add_to_local_data(id, card,dict){
-    let ld = localStorage
-    ld.setItem(id, card);
+    localStorage.setItem('CardData', JSON.stringify(cards))
+    localStorage.setItem(id, JSON.stringify(card));
 
     let img = document.getElementById(id+"-image")
-    img.style.opacity = 1;
+    img.style.opacity = '1';
 
     let rem  = document.getElementById('rem-btn-'+ id);
     rem.removeAttribute('disabled');
@@ -26,40 +32,129 @@ function add_to_local_data(id, card,dict){
     add.setAttribute('disabled', '')
 }
 
-function remove_from_local_data(id){
-    let ld = localStorage
-    ld.removeItem(id);
+function remove_from_local_data(id ){
+    let cards = JSON.parse(localStorage['CardData']);
+    cards[id]['collected'] = 'false'
+    localStorage.setItem('CardData', JSON.stringify(cards))
 
     let img = document.getElementById(id+"-image")
-    img.style.opacity = .5;
+    img.style.opacity = '.5';
 
     let rem  = document.getElementById('rem-btn-' + id);
     rem.setAttribute('disabled', '')
 
     let add  = document.getElementById('add-btn-' + id);
     add.removeAttribute('disabled');
+    localStorage.removeItem(id);
 }
 
-//display info when page finishes loading
-document.addEventListener("DOMContentLoaded", function(){
-    let parent = document.getElementById('collection-display')
-    let collection_keys = Object.keys(localStorage);
-
+function create_card_display(deck){ // rename this function
+    let code = deck
+    let parent = document.getElementById('cards-page-display')
+    let cards = JSON.parse(localStorage['CardData']);
+    let keys = Object.keys(cards)
     const row = document.createElement('div');
     row.setAttribute('class',"row");
-    for(let key of collection_keys ){
-        if(key !== 'CardData'){
-            let x = JSON.parse(localStorage.getItem(key));
-            card = create_card(x, key)
-            parent.append(card)
+    if(parent != null){
+        for(let key of keys){
+            if(cards[key]['deck'] === code || cards[key]['name'].includes(code)){
+                let title = document.createElement('h1')
+                title.setAttribute('id', 'felipe')
+                row.append(title)
+                let x = cards[key];
+                let card = create_display_card(x, key, cards)
+                parent.append(card)
+                document.getElementById('p-head').innerHTML = `Deck: ${code}`
+            }
         }
     }
-});
 
-function create_card(x, i  ){
+}
+
+function create_display_card(x, i, cards){ // card in
     let card = document.createElement('div');
             card.setAttribute('id', `${x['card_code']}`);
-            card.setAttribute('class', 'card collected col-sm-4 col-lg-2');
+            card.setAttribute('class', 'card collected col-sm-2 col-lg-2');
+    let card_header = document.createElement('div');
+        card_header.setAttribute('class', 'card-header');
+        card_header.innerText = `${x['card_code']}  ${x['name']}`;
+    let card_image_div = document.createElement('div');
+        card_image_div.setAttribute('class', 'card-image')
+
+    let card_image = document.createElement('img');
+
+        card_image.setAttribute('id', `${i}-image`)
+        card_image.setAttribute('class', 'img-thumbnail popover-image');
+        card_image.setAttribute('data-bs-toggle', 'modal');
+        card_image.setAttribute('data-bs-target',`#${i}-Modal`)
+        card_image.setAttribute('alt', `image of ${x['name']}`)
+        card_image.setAttribute('src', `static/Decks/${x["deck"]}/CardImages/${x["images"]}`)
+        card_image_div.append(card_image)
+    let card_footer = document.createElement('div')
+        card_footer.setAttribute('class', 'card-footer')
+    let card_row = document.createElement('div')
+        card_row.setAttribute('class', 'row')
+    let card_button_col = document.createElement('div')
+        card_button_col.setAttribute('class', 'col text-center')
+    let card_button_form = document.createElement('form')
+
+    let add_card_button = document.createElement('button')
+        add_card_button.innerText = 'ADD'
+        add_card_button.setAttribute('class', 'btn btn-success')
+        add_card_button.setAttribute('type', 'button')
+        add_card_button.setAttribute('id', `add-btn-${i}`)
+        add_card_button.setAttribute('onclick', `add_to_local_data( '${i}', ${JSON.stringify(cards[i])})`)
+
+    let remove_card_button = document.createElement('button')
+        remove_card_button.innerText = 'REMOVE'
+        remove_card_button.setAttribute('class', 'btn btn-danger')
+        remove_card_button.setAttribute('id', `rem-btn-${i}`)
+        remove_card_button.setAttribute('name', 'collection')
+        remove_card_button.setAttribute('onclick', ` remove_from_local_data("${i}" )`)
+        add_card_button.setAttribute('name', 'collection')
+        remove_card_button.setAttribute('type', 'button')
+
+    if(x['collected'] === 'false') {
+        card_image.style.opacity = '.5';
+        remove_card_button.setAttribute('disabled', '')
+    }else{
+        add_card_button.setAttribute('disabled','')
+    }
+
+    card_button_form.append(add_card_button)
+    card_button_form.append(remove_card_button)
+    card_button_col.append(card_button_form)
+    card_row.append(card_button_col)
+    card_footer.append(card_row)
+
+    card.append(card_header);
+    card.append(card_image_div)
+    card.append(card_footer)
+
+    return card
+}
+
+function create_collection(){
+    let parent = document.getElementById('collection-display')
+    let collection_keys = Object.keys(localStorage);
+    collection_keys.sort()
+    const row = document.createElement('div');
+    row.setAttribute('class',"row");
+    if(parent != null){
+        for (let key of collection_keys) {
+            if (key !== 'CardData') {
+                let x = JSON.parse(localStorage.getItem(key));
+                let card = create_card(x, key)
+                parent.append(card)
+            }
+        }
+    }
+}
+
+function create_card(x, i){
+    let card = document.createElement('div');
+            card.setAttribute('id', `${i}`);
+            card.setAttribute('class', 'card collected col-sm-3 col-lg-3 ');
     let card_header = document.createElement('div');
         card_header.setAttribute('class', 'card-header');
         card_header.innerText = `${x['card_code']}  ${x['name']}`;
@@ -81,7 +176,7 @@ function create_card(x, i  ){
     let card_button_form = document.createElement('form')
     let card_button = document.createElement('button')
         card_button.setAttribute('type', 'button')
-        card_button.setAttribute('onclick', `collection_remove_from_local_data("${x['card_code']}")`)
+        card_button.setAttribute('onclick', `collection_remove_from_local_data("${i}")`)
         card_button.setAttribute('id', 'collection-button')
         card_button.setAttribute('class', 'btn btn-danger')
         card_button.setAttribute('name', 'collection')
@@ -99,13 +194,20 @@ function create_card(x, i  ){
     return card
 }
 
-function collection_remove_from_local_data(id){
+function collection_remove_from_local_data(id){ // THIS IS USED DESPITE IT SAYING ITS NOT BEING USED
+    let cards = JSON.parse(localStorage['CardData']);
     console.log(id)
-    let ld = localStorage
-    ld.removeItem(id);
-
+    cards[id]['collected'] = 'false'
+    localStorage.setItem('CardData', JSON.stringify(cards))
     let card  = document.getElementById(id);
+
+    localStorage.removeItem(id);
     card.style.display = 'none'
+}
+
+function get_local_data(){
+    let x = document.getElementById('return-current-collection')
+    x.value = Object.keys(localStorage)
 }
 
 
